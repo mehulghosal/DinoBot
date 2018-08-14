77fac398b0c68285a3140e37c8cb6865a91485f0
mehulghosal <mehul.ghosal@gmail.com>
2018-08-14 01:16:44 -0400

    earoifh

--- dinoBot.py
+++ dinoBot.py
@@ -1,6 +1,6 @@
 import math, keyboard
 from time import sleep
-from PIL import *
+from PIL import ImageGrab, Image
 
 def startGame():
 	while True:
@@ -27,7 +27,36 @@ def duck():
 topRight = (425, 270)
 bottomRight = (425, 308)
 
-white = (255, 255, 255)
-grey = (83, 83, 83)
+#gameover (white back)
+goSumW = 1475832
+#gameover black back)
+goSumB = 160512
 
-duck()
+lowobject = (list(ImageGrab.grab((470, 280, 580, 306)).getdata()))
+lowobjectSum = 0
+for i in lowobject: lowobjectSum += sum(i)
+print(lowobjectSum)
+
+highobject = (list(ImageGrab.grab((470, 265, 580, 280)).getdata()))
+highobjectSum = 0
+for i in highobject: highobjectSum += sum(i)
+print((highobjectSum))
+
+gameOverCheck = 0
+
+
+# startGame()
+
+# while True:
+
+# 	gameOverImage = (list(ImageGrab.grab((588, 218, 780, 229)).getdata()))
+# 	gameOverCheck = 0
+# 	for i in gameOverImage: gameOverCheck += sum(i)
+# 	print(gameOverCheck)
+
+# 	if 1450000 < gameOverCheck < 1500000 or 150000 < gameOverCheck < 170000:
+# 		print("break")
+# 		break
+
+# 	#515,280 --> +109, +26
+# 	lowobject = (list(ImageGrab.grab((515, 280, 624, 306)).getdata()))
